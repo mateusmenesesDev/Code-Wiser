d3c522472e4ca6a08432c7dbc396581bba22c2b0
Mateus <mateusppa10@gmail.com>
2024-11-20 19:00:23 -0300

    💄 Feat: update favicon

--- src/app/(system)/layout.tsx
+++ src/app/(system)/layout.tsx
@@ -1,18 +1,7 @@
 import { auth } from '@clerk/nextjs/server';
-import type { Metadata } from 'next';
 import Header from '~/common/layout/Header';
 import Sidebar from '~/common/layout/SideBar';
 import '~/styles/globals.css';
-
-export const metadata: Metadata = {
-	title: {
-		template: '%s | Code Wise',
-		default: 'Code Wise'
-	},
-	description:
-		'Code Wise - The best way to learn how to code with senior developers',
-	icons: [{ rel: 'icon', url: '/favicon.ico' }]
-};
 
 export default function Layout({
 	children
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { auth } from '@clerk/nextjs/server';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { Toaster } from 'sonner';
 import { SyncActiveOrganization } from '~/features/auth/components/SyncActiveOrganizations';
@@ -12,6 +13,16 @@ import { ThemeProvider } from '~/providers/ThemeProvider';
 import { TRPCReactProvider } from '~/trpc/react';
 
 dayjs.extend(localizedFormat);
+
+export const metadata: Metadata = {
+	title: {
+		template: '%s | Code Wise',
+		default: 'Code Wise'
+	},
+	description:
+		'Code Wise - The best way to learn how to code with senior developers',
+	icons: [{ rel: 'icon', url: '/favicon.svg' }]
+};
 
 export default function RootLayout({
 	children
