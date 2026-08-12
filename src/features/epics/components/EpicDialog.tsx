2b70088bcdc8d58314bc44eed8a7348045f52488
Mateus <mateusmenesesdev@gmail.com>
2026-08-12 19:29:53 -0300

    fix: accept empty epic selection

--- src/features/epics/components/EpicDialog.tsx
+++ src/features/epics/components/EpicDialog.tsx
@@ -29,7 +29,7 @@ import type { EpicApiOutput, EpicInput } from '../types/Epic.type';
 
 interface EpicDialogProps {
 	projectId: string;
-	epic?: EpicApiOutput;
+	epic?: EpicApiOutput | null;
 	isTemplate?: boolean;
 }
 
