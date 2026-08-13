9f27827d6c7ab7ffc62c8e35475affac8a060899
Mateus <mateusmenesesdev@gmail.com>
2026-08-13 07:50:45 -0300

    fix(migrations): unlock agenda tables on CockroachDB

--- prisma/migrations/20260813072000_p1_2_task_agenda/migration.sql
+++ prisma/migrations/20260813072000_p1_2_task_agenda/migration.sql
@@ -1,11 +1,14 @@
+ALTER TABLE "User" SET (schema_locked = false);
 ALTER TABLE "User"
 ADD COLUMN "taskDeadlineRemindersEnabled" BOOL NOT NULL DEFAULT true;
+ALTER TABLE "User" SET (schema_locked = true);
 
+ALTER TABLE "Notification" SET (schema_locked = false);
 ALTER TABLE "Notification"
 ADD COLUMN "dedupeKey" STRING;
-
 CREATE UNIQUE INDEX "Notification_dedupeKey_key"
 ON "Notification"("dedupeKey");
+ALTER TABLE "Notification" SET (schema_locked = true);
 
 ALTER TYPE "NotificationType" ADD VALUE 'TASK_DUE_SOON';
 ALTER TYPE "NotificationType" ADD VALUE 'TASK_OVERDUE';
