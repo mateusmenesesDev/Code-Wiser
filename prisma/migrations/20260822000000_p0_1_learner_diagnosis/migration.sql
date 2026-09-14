e8b44ae279d95b07f520fff7d3fcc2ec4d526385
Mateus <mateusmenesesdev@gmail.com>
2026-09-14 13:24:22 -0300

    fix: unlock User during diagnosis migration

--- prisma/migrations/20260822000000_p0_1_learner_diagnosis/migration.sql
+++ prisma/migrations/20260822000000_p0_1_learner_diagnosis/migration.sql
@@ -1,3 +1,4 @@
+ALTER TABLE "public"."User" SET (schema_locked = false);
 ALTER TABLE "public"."User"
 ADD COLUMN "learningGoal" STRING,
 ADD COLUMN "selfReportedLevel" STRING,
@@ -5,3 +6,4 @@ ADD COLUMN "interestedTechnologies" STRING[] NOT NULL DEFAULT ARRAY[]::STRING[],
 ADD COLUMN "weeklyAvailabilityBand" STRING,
 ADD COLUMN "priorExperience" STRING,
 ADD COLUMN "diagnosisCompletedAt" TIMESTAMP(3);
+ALTER TABLE "public"."User" SET (schema_locked = true);
